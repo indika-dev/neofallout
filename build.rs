7bc9fabd3db75d7d494004f81153e0f375d485b5
Stefan Maaßen <stefan.maassen@posteo.de>
2023-09-10 15:38:59 +0200

    added more bindings

--- build.rs
+++ build.rs
@@ -19,7 +19,16 @@ fn main() {
 
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    add_art_h_to_bindings(out_path);
+    add_art_h_to_bindings(&out_path);
+    add_autorun_h_to_bindings(&out_path);
+    add_color_h_to_bindings(&out_path);
+    add_credits_h_to_bindings(&out_path);
+    add_cycle_h_to_bindings(&out_path);
+    add_db_h_to_bindings(&out_path);
+    add_debug_h_to_bindings(&out_path);
+    add_draw_h_to_bindings(&out_path);
+    add_endgame_h_to_bindings(&out_path);
+    add_game_h_to_bindings(&out_path);
 }
 
 fn assign_defaults(builder: bindgen::Builder) -> bindgen::Builder {
@@ -40,8 +49,8 @@ fn assign_defaults(builder: bindgen::Builder) -> bindgen::Builder {
     builder_with_defaults
 }
 
-fn write_bindings(sourcefile: &str, builder: bindgen::Builder, out_path: PathBuf) {
-    let bindings = assign_defaults(builder)
+fn write_bindings(sourcefile: &str, builder: &bindgen::Builder, out_path: PathBuf) {
+    let bindings = assign_defaults(builder.clone())
         .generate()
         .expect("Unable to generate bindings");
     bindings
@@ -52,7 +61,249 @@ fn write_bindings(sourcefile: &str, builder: bindgen::Builder, out_path: PathBuf
         .expect("Couldn't write bindings!");
 }
 
-fn add_art_h_to_bindings(out_path: PathBuf) {
+fn add_game_h_to_bindings(out_path: &PathBuf) -> bindgen::Builder {
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let builder = bindgen::Builder::default()
+        .header("fallout2-ce/src/game.h")
+        .allowlist_type("fallout::GameState")
+        .allowlist_type("fallout::MessageList")
+        .allowlist_type("fallout::GameMode")
+        .allowlist_type("fallout::GameMode::Flags")
+        .allowlist_type("fallout::ScopedGameMode")
+        .allowlist_function("fallout::gameInitWithOptions")
+        .allowlist_function("fallout::gameReset")
+        .allowlist_function("fallout::gameExit")
+        .allowlist_function("fallout::gameHandleKey")
+        .allowlist_function("fallout::gameUiDisable")
+        .allowlist_function("fallout::gameUiEnable")
+        .allowlist_function("fallout::gameUiIsDisabled")
+        .allowlist_function("fallout::gameGetGlobalVar")
+        .allowlist_function("fallout::gameSetGlobalVar")
+        .allowlist_function("fallout::globalVarsRead")
+        .allowlist_function("fallout::gameGetState")
+        .allowlist_function("fallout::gameRequestState")
+        .allowlist_function("fallout::gameUpdateState")
+        .allowlist_function("fallout::showQuitConfirmationDialog")
+        .allowlist_function("fallout::gameShowDeathDialog")
+        .allowlist_function("fallout::gameGetGlobalPointer")
+        .allowlist_function("fallout::gameSetGlobalPointer");
+    write_bindings("game_h", &builder, out_path.clone());
+    return builder;
+
+    // extern int* gGameGlobalVars;
+    // extern int gGameGlobalVarsLength;
+    // extern const char* asc_5186C8;
+    // extern int _game_user_wants_to_quit;
+}
+
+fn add_endgame_h_to_bindings(out_path: &PathBuf) -> bindgen::Builder {
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let builder = bindgen::Builder::default()
+        .header("fallout2-ce/src/endgame.h")
+        .allowlist_type("fallout::EndgameDeathEndingReason")
+        .allowlist_function("fallout::endgamePlaySlideshow")
+        .allowlist_function("fallout::endgamePlayMovie")
+        .allowlist_function("fallout::endgameDeathEndingInit")
+        .allowlist_function("fallout::endgameDeathEndingExit")
+        .allowlist_function("fallout::endgameSetupDeathEnding")
+        .allowlist_function("fallout::endgameDeathEndingGetFileName");
+    write_bindings("endgame_h", &builder, out_path.clone());
+    return builder;
+}
+
+fn add_draw_h_to_bindings(out_path: &PathBuf) -> bindgen::Builder {
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let builder = bindgen::Builder::default()
+        .header("fallout2-ce/src/draw.h")
+        .allowlist_function("fallout::bufferDrawLine")
+        .allowlist_function("fallout::bufferDrawRect")
+        .allowlist_function("fallout::bufferDrawRectShadowed")
+        .allowlist_function("fallout::blitBufferToBufferStretch")
+        .allowlist_function("fallout::blitBufferToBufferStretchTrans")
+        .allowlist_function("fallout::blitBufferToBuffer")
+        .allowlist_function("fallout::blitBufferToBufferTrans")
+        .allowlist_function("fallout::bufferFill")
+        .allowlist_function("fallout::_buf_texture")
+        .allowlist_function("fallout::_lighten_buf")
+        .allowlist_function("fallout::_swap_color_buf")
+        .allowlist_function("fallout::bufferOutline")
+        .allowlist_function("fallout::srcCopy")
+        .allowlist_function("fallout::transSrcCopy");
+    write_bindings("draw_h", &builder, out_path.clone());
+    return builder;
+}
+
+fn add_debug_h_to_bindings(out_path: &PathBuf) -> bindgen::Builder {
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let builder = bindgen::Builder::default()
+        .header("fallout2-ce/src/debug.h")
+        .allowlist_function("fallout::_GNW_debug_init")
+        .allowlist_function("fallout::_debug_register_mono")
+        .allowlist_function("fallout::_debug_register_log")
+        .allowlist_function("fallout::_debug_register_screen")
+        .allowlist_function("fallout::_debug_register_env")
+        .allowlist_function("fallout::_debug_register_func")
+        .allowlist_function("fallout::debugPrint")
+        .allowlist_function("fallout::_debug_exit");
+    write_bindings("debug_h", &builder, out_path.clone());
+    return builder;
+}
+
+fn add_db_h_to_bindings(out_path: &PathBuf) -> bindgen::Builder {
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let builder = bindgen::Builder::default()
+        .header("fallout2-ce/src/db.h")
+        .allowlist_function("fallout::dbOpen")
+        .allowlist_function("fallout::_db_total")
+        .allowlist_function("fallout::dbExit")
+        .allowlist_function("fallout::dbGetFileSize")
+        .allowlist_function("fallout::dbGetFileContents")
+        .allowlist_function("fallout::fileClose")
+        .allowlist_function("fallout::fileOpen")
+        .allowlist_function("fallout::filePrintFormatted")
+        .allowlist_function("fallout::fileReadChar")
+        .allowlist_function("fallout::fileReadString")
+        .allowlist_function("fallout::fileWriteString")
+        .allowlist_function("fallout::fileRead")
+        .allowlist_function("fallout::fileWrite")
+        .allowlist_function("fallout::fileSeek")
+        .allowlist_function("fallout::fileTell")
+        .allowlist_function("fallout::fileRewind")
+        .allowlist_function("fallout::fileEof")
+        .allowlist_function("fallout::fileReadUInt8")
+        .allowlist_function("fallout::fileReadInt16")
+        .allowlist_function("fallout::fileReadUInt16")
+        .allowlist_function("fallout::fileReadInt32")
+        .allowlist_function("fallout::fileReadUInt32")
+        .allowlist_function("fallout::_db_freadInt")
+        .allowlist_function("fallout::fileReadFloat")
+        .allowlist_function("fallout::fileReadBool")
+        .allowlist_function("fallout::fileWriteUInt8")
+        .allowlist_function("fallout::fileWriteInt16")
+        .allowlist_function("fallout::fileWriteUInt16")
+        .allowlist_function("fallout::fileWriteInt32")
+        .allowlist_function("fallout::_db_fwriteLong")
+        .allowlist_function("fallout::fileWriteUInt32")
+        .allowlist_function("fallout::fileWriteFloat")
+        .allowlist_function("fallout::fileWriteBool")
+        .allowlist_function("fallout::fileReadUInt8List")
+        .allowlist_function("fallout::fileReadFixedLengthString")
+        .allowlist_function("fallout::fileReadInt16List")
+        .allowlist_function("fallout::fileReadUInt16List")
+        .allowlist_function("fallout::fileReadInt32List")
+        .allowlist_function("fallout::_db_freadIntCount")
+        .allowlist_function("fallout::fileReadUInt32List")
+        .allowlist_function("fallout::fileWriteUInt8List")
+        .allowlist_function("fallout::fileWriteFixedLengthString")
+        .allowlist_function("fallout::fileWriteInt16List")
+        .allowlist_function("fallout::fileWriteUInt16List")
+        .allowlist_function("fallout::fileWriteInt32List")
+        .allowlist_function("fallout::_db_fwriteLongCount")
+        .allowlist_function("fallout::fileWriteUInt32List")
+        .allowlist_function("fallout::fileNameListInit")
+        .allowlist_function("fallout::fileNameListFree")
+        .allowlist_function("fallout::fileGetSize")
+        .allowlist_function("fallout::fileSetReadProgressHandler");
+    write_bindings("db_h", &builder, out_path.clone());
+    return builder;
+}
+
+fn add_cycle_h_to_bindings(out_path: &PathBuf) -> bindgen::Builder {
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let builder = bindgen::Builder::default()
+        .header("fallout2-ce/src/cycle.h")
+        .allowlist_function("fallout::colorCycleInit")
+        .allowlist_function("fallout::colorCycleReset")
+        .allowlist_function("fallout::colorCycleFree")
+        .allowlist_function("fallout::colorCycleDisable")
+        .allowlist_function("fallout::colorCycleEnable")
+        .allowlist_function("fallout::colorCycleEnabled")
+        .allowlist_function("fallout::cycleSetSpeedFactor")
+        .allowlist_function("fallout::colorCycleTicker");
+    write_bindings("cycle_h", &builder, out_path.clone());
+    return builder;
+}
+
+fn add_credits_h_to_bindings(out_path: &PathBuf) -> bindgen::Builder {
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let builder = bindgen::Builder::default()
+        .header("fallout2-ce/src/credits.h")
+        .allowlist_function("fallout::creditsOpen");
+    write_bindings("credits_h", &builder, out_path.clone());
+    return builder;
+}
+
+fn add_color_h_to_bindings(out_path: &PathBuf) -> bindgen::Builder {
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let builder = bindgen::Builder::default()
+        .header("fallout2-ce/src/color.h")
+        .allowlist_type("fallout::MallocProc")
+        .allowlist_type("fallout::ReallocProc")
+        .allowlist_type("fallout::FreeProc")
+        .allowlist_function("fallout::colorPaletteSetFileIO")
+        .allowlist_function("fallout::_calculateColor")
+        .allowlist_function("fallout::Color2RGB")
+        .allowlist_function("fallout::colorPaletteFadeBetween")
+        .allowlist_function("fallout::colorPaletteSetTransitionCallback")
+        .allowlist_function("fallout::_setSystemPalette")
+        .allowlist_function("fallout::_getSystemPalette")
+        .allowlist_function("fallout::_setSystemPaletteEntries")
+        .allowlist_function("fallout::_getColorBlendTable")
+        .allowlist_function("fallout::_freeColorBlendTable")
+        .allowlist_function("fallout::colorPaletteSetMemoryProcs")
+        .allowlist_function("fallout::colorSetBrightness")
+        .allowlist_function("fallout::colorPaletteLoad")
+        .allowlist_function("fallout::colorPushColorPalette")
+        .allowlist_function("fallout::_colorError")
+        .allowlist_function("fallout::colorPopColorPalette")
+        .allowlist_function("fallout::_initColors")
+        .allowlist_function("fallout::_colorsClose");
+    write_bindings("color_h", &builder, out_path.clone());
+    return builder;
+}
+
+fn add_character_selector_h_to_bindings(out_path: &PathBuf) -> bindgen::Builder {
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let builder = bindgen::Builder::default()
+        .header("fallout2-ce/src/character_selector.h")
+        .allowlist_function("fallout::characterSelectorOpen")
+        .allowlist_function("fallout::premadeCharactersInit")
+        .allowlist_function("fallout::premadeCharactersExit");
+    write_bindings("character_selector_h", &builder, out_path.clone());
+    return builder;
+}
+
+fn add_autorun_h_to_bindings(out_path: &PathBuf) -> bindgen::Builder {
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let builder = bindgen::Builder::default()
+        .header("fallout2-ce/src/autorun.h")
+        .allowlist_function("fallout::autorunMutexCreate")
+        .allowlist_function("fallout::autorunMutexClose");
+    write_bindings("autorun_h", &builder, out_path.clone());
+    return builder;
+}
+
+fn add_art_h_to_bindings(out_path: &PathBuf) -> bindgen::Builder {
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
@@ -104,5 +355,6 @@ fn add_art_h_to_bindings(out_path: PathBuf) {
         .allowlist_function("fallout::artLoad")
         .allowlist_function("fallout::artRead")
         .allowlist_function("fallout::artWrite");
-    write_bindings("art_h", builder, out_path);
+    write_bindings("art_h", &builder, out_path.clone());
+    return builder;
 }
